29f7744b05f6f1b7b296b7ac786046b1bd712a5e
mulyonoputra <mulyonoputra1994@gmail.com>
2020-10-03 21:08:29 +0700

    add cards to OurService component

--- src/components/page/our-service/OurService.js
+++ src/components/page/our-service/OurService.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import '../../../App.css';
+import CardItem from '../../shared/cards/CardItem';
 
 function OurService() {
 	return (
 		<div>
-			<h1 className="services"> Hello World </h1>
+			<br/>
+			<div>
+			<ul className='cards__items'>
+            <CardItem
+              src='/assets/images/_MG_8783.JPG'
+              text='Explores the hidden waterfall deep inside the Amazon Jungle'
+              label='Wedding'
+              path='/wedding'
+            />
+            <CardItem
+              src='/assets/images/photo4.jpg'
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Prewedding'
+              path='/prewedding'
+            />
+          </ul>
+			</div>
 		</div>
 	);
 }
