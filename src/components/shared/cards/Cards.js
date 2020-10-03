ea01ede9e91b7df36d51e55897eda00027c88eea
mulyonoputra <mulyonoputra1994@gmail.com>
2020-10-03 21:07:50 +0700

    add images to cards

--- src/components/shared/cards/Cards.js
+++ src/components/shared/cards/Cards.js
@@ -1,44 +1,46 @@
 import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
+import * as Constants from '../../shared/Constants' 
 
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Kelvin Studio</h1>
+      <h4 className="article">{Constants.ARTICLE}</h4>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
+              src='/assets/images/_MG_8783.JPG'
+              text='Explores the hidden waterfall deep inside the Amazon Jungle'
+              label='Wedding'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
+              src='/assets/images/photo3.jpeg'
               text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
+              label='Wedding'
               path='/services'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              src='/assets/images/_MG_4204.jpg'
               text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
+              label='Wedding'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
+              src='/assets/images/DSC04353.JPG'
               text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
+              label='Wedding'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
+              src='/assets/images/photo4.jpg'
               text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
+              label='Prewedding'
               path='/sign-up'
             />
           </ul>
