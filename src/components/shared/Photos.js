26013ba723a2af6a06c0372942827cf2287d66f9
mulyonoputra <mulyonoputra1994@gmail.com>
2020-10-06 00:31:14 +0700

    Code Formatter

--- src/components/shared/Photos.js
+++ src/components/shared/Photos.js
@@ -1,155 +1,35 @@
-
-
 export const preWeddingGallery = [
-  {
-    src: '/assets/images/wedding4/DSC03700.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/_MG_7957.jpg',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/_MG_8056.jpg',
-    width: 4,
-    height: 5,
-  },
-  {
-    src: '/assets/images/_MG_8075.jpg',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/_MG_8581.jpg',
-    width: 4,
-    height: 5,
-  },
-  {
-    src: '/assets/images/_MG_8608.jpg',
-    width: 4,
-    height: 5,
-  },
-  {
-    src: '/assets/images/_MG_8888.jpg',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/_MG_8954.jpg',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/_MG_8967.jpg',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/_MG_8971.jpg',
-    width: 4,
-    height: 3,
-  }
-
-]
+	{ src: '/assets/images/_MG_7957.jpg', width: 4, height: 3 },
+	{ src: '/assets/images/_MG_8056.jpg', width: 4, height: 5 },
+	{ src: '/assets/images/_MG_8075.jpg', width: 4, height: 3 },
+	{ src: '/assets/images/_MG_8581.jpg', width: 4, height: 5 },
+	{ src: '/assets/images/_MG_8608.jpg', width: 4, height: 5 },
+	{ src: '/assets/images/_MG_8888.jpg', width: 4, height: 3 },
+	{ src: '/assets/images/_MG_8954.jpg', width: 4, height: 3 },
+	{ src: '/assets/images/_MG_8967.jpg', width: 4, height: 3 },
+	{ src: '/assets/images/_MG_8971.jpg', width: 4, height: 3 }
+];
 
 export const weddingGallery04 = [
-  {
-    src: '/assets/images/wedding4/DSC03700.JPG',
-    width: 4,
-    height: 5,
-  },
-  {
-    src: '/assets/images/wedding4/DSC03709.JPG',
-    width: 4,
-    height: 5,
-  },
-  {
-    src: '/assets/images/wedding4/DSC03770.JPG',
-    width: 4,
-    height: 5,
-  },
-  {
-    src: '/assets/images/wedding4/DSC03812.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04020.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04188.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04195.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04205.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04216.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04230.JPG',
-    width: 4,
-    height: 5,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04244.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04353.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04383.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04408.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04491.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04493.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04603.JPG',
-    width: 4,
-    height: 3,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04614.JPG',
-    width: 4,
-    height: 5,
-  },
-  {
-    src: '/assets/images/wedding4/DSC04617.JPG',
-    width: 4,
-    height: 3,
-  },
-]
+	{ src: '/assets/images/wedding4/DSC03700.JPG', width: 4,	height: 5 },
+  { src: '/assets/images/wedding4/DSC03709.JPG',width: 4, height: 5 },
+  { src: '/assets/images/wedding4/DSC03770.JPG',width: 4, height: 5 },
+	{ src: '/assets/images/wedding4/DSC03812.JPG',width: 4, height: 3 },
+	{ src: '/assets/images/wedding4/DSC04020.JPG',width: 4, height: 3 },
+	{ src: '/assets/images/wedding4/DSC04188.JPG',width: 4, height: 3 },
+	{ src: '/assets/images/wedding4/DSC04195.JPG',width: 4, height: 3 },
+	{ src: '/assets/images/wedding4/DSC04205.JPG',width: 4, height: 3 },
+	{ src: '/assets/images/wedding4/DSC04216.JPG',width: 4, height: 3 },
+	{ src: '/assets/images/wedding4/DSC04230.JPG',width: 4, height: 5 },
+	{ src: '/assets/images/wedding4/DSC04244.JPG',width: 4, height: 3 },
+	{	src: '/assets/images/wedding4/DSC04353.JPG',width: 4, height: 3 },
+	{ src: '/assets/images/wedding4/DSC04383.JPG',width: 4, height: 3 },
+	{ src: '/assets/images/wedding4/DSC04408.JPG',width: 4, height: 3 },
+	{ src: '/assets/images/wedding4/DSC04491.JPG',width: 4, height: 3 },
+	{ src: '/assets/images/wedding4/DSC04493.JPG',width: 4, height: 3 },
+	{ src: '/assets/images/wedding4/DSC04603.JPG',width: 4, height: 3 },
+	{ src: '/assets/images/wedding4/DSC04614.JPG',width: 4, height: 5 },
+	{ src: '/assets/images/wedding4/DSC04617.JPG',width: 4, height: 3 }
+];
 
 /* _MG_4204.jpg */
