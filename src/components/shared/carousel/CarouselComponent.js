ec34516082b3de14d1885ffc99cafc560eb0cbfe
Mulyono Putra <mulyonoputra1994@gmail.com>
2021-01-03 12:04:33 +0700

    Change image carousel.

--- src/components/shared/carousel/CarouselComponent.js
+++ src/components/shared/carousel/CarouselComponent.js
@@ -9,21 +9,21 @@ function CarouselComponent() {
                 <Carousel.Item interval={1000}>
                     <img
                         className="d-block w-100 h-20"
-                        src="/assets/images/_MG_7957.jpg"
+                        src="/assets/images/carousel/Carousel1.jpg"
                         alt=""
                     />
                 </Carousel.Item>
                 <Carousel.Item interval={500}>
                     <img
                         className="d-block w-100"
-                        src="/assets/images/wedding2/DSC01786.jpg"
+                        src="/assets/images/carousel/Carousel2.jpg"
                         alt=""
                     />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src="/assets/images/prewedding3/DSCF4666.jpg"
+                        src="/assets/images/carousel/Carousel3.jpg"
                         alt=""
                     />
                 </Carousel.Item>
